915e235f1e8af2e531bc8c55b98e1ae84effe99b
Soni54 <sonisingh90930@gmail.com>
2024-08-08 19:05:35 +0530

    Update Halfdiamond.java

--- Halfdiamond.java
+++ Halfdiamond.java
@@ -6,7 +6,7 @@ class Halfdiamond{
         }
         System.out.println();
     }
-    for(int i=5;i>=1;i--){
+    for(int i=4;i>=1;i--){
       for(int j=1;j<=i;j++){
           System.out.print("*");
       }
